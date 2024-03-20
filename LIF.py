5e4a75373955eff238f0e039c6f366c35c50dee8
Christos Iosif <ciosif01@ucy.ac.cy>
2024-03-20 15:18:25 +0200

    fixed weird firing and added refractory period

--- LIF.py
+++ LIF.py
@@ -1,24 +1,58 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 # Constants ----------------------------------------------------------------
 tm = 30 # in msec
 EL = -65 # =Vreset in mV
 Vth = -50 # in mv
 Rm = 90 # in MOhms
 Vzero = -67
+Vreset = EL
+Trefractory = 200 # in msec
 
-Ie = float (input("Enter the current in nA: "))
+#Ie = float (input("Enter the current in nA: "))
+Ie = 1
 
-duration = 10000000000   # each iteration represents a ms
-V = []
-time = []
-for t in range(0, duration):
-    Vt = EL + Rm * Ie + (Vzero - EL - Rm*Ie)*np.exp(-t/tm)
+duration = 3000   # each iteration represents a ms
+V = np.zeros(duration)
+time = np.zeros(duration)
+cnt = 1000
+V[0] = Vzero
 
-    V.append(Vt)
-    time.append(t)
-    print("V(" + str(t) + ") = " + str(V[t]))
-    t+=1
+sum = Vreset
+t=1
+
+i=1
+for t in range(1, duration):
+
+    if (cnt <= Trefractory):
+        cnt += 1
+        V[t] = Vreset
+        sum = Vreset
+        time[t] = t
+        continue
+    
+    V[t] = (EL + Rm * Ie + (Vzero - EL - Rm*Ie)*np.exp(-i/tm))
+    #print(str((Vzero - EL - Rm*Ie)*np.exp(-t/tm)) +"    " + str(EL + Rm * Ie))
+    sum += (V[t])
+  
+    i+=1
+    
+    if (sum >= Vth):
+        V[t] = Vreset
+        sum = Vreset
+        i=1
+        #print("hello " + str(t))
+        cnt = 1
+    
+
+    #print (V[t])   
+    #if (Rm*Ie>Vth-EL):
+    #    tisi = tm * math.log((Rm*Ie)/(Rm*Ie+EL-Vth))
+    #    Vt = EL + Rm * Ie + (-Rm*Ie)*np.exp(-tisi/tm)
+
+    time[t] = t
+
 
 plt.plot(time, V)
 plt.xlabel('Time (ms)')
