b6b84259b94050c859c39f5c8b64ebc75fa69267
Christos Iosif <ciosif01@ucy.ac.cy>
2024-03-22 16:20:03 +0200

    Added statistics

--- LIF.py
+++ LIF.py
@@ -8,12 +8,12 @@ Vth = -50 # in mv
 Rm = 90 # in MOhms
 Vzero = -67
 Vreset = EL
-Trefractory = 50 # in msec
-duration = 200   # each iteration represents a ms
+Trefractory = 2 # in msec
+duration = 1000   # each iteration represents a ms
 # ----------------------------------------------------------------
 
-#Ie = float (input("Enter the current in nA: "))
-Ie = 1
+Ie = float (input("Enter the current in nA: "))
+#Ie = 1
 
 # Initializations
 V = np.zeros(duration)
@@ -21,6 +21,9 @@ time = np.zeros(duration)
 refCount = 1000
 V[0] = Vzero
 i=1
+firstSpike = 0
+risi = 0
+spikeCount = 0
 
 # run simulation
 for t in range(1, duration):
@@ -35,24 +38,35 @@ for t in range(1, duration):
   
   
     i+=1
+
+    
+        
+        
     
     if (V[t] >= Vth):
         #V[t] = Vreset
         i=1
         refCount = 1
+        spikeCount += 1
+        risi += 1/(tm * math.log((Rm*Ie)/(Rm*Ie+EL-Vth)))
+        if (firstSpike == 0):
+            firstSpike = t
     
     time[t] = t
-    #print (V[t])   
-    #if (Rm*Ie>Vth-EL):
-    #    tisi = tm * math.log((Rm*Ie)/(Rm*Ie+EL-Vth))
-    #    Vt = EL + Rm * Ie + (-Rm*Ie)*np.exp(-tisi/tm)
-
+  
     
 
+    
+print("First spike at t=" + str(firstSpike))        # question 1
+if spikeCount == 0:
+    print("The neuron did not fire, so there is no isi")
+else:
+    print("Average isi: " + str(risi/spikeCount))      # question 2
+print("Spike count: " + str(spikeCount))            # question 3
 
 plt.plot(time, V)
 plt.xlabel('Time (ms)')
 plt.ylabel('Membrane Potential (mV)')
-plt.title('Leaky Integrate and Fire Neuron')
+plt.title('Leaky Integrate and Fire Neuron with constant current I=' + str(Ie))
 plt.grid(True)
 plt.show()
